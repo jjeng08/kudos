0b662172d89b7e2b1391d887aead0ee0ce42572a
jjeng08 <jjeng08@users.noreply.github.com>
2018-11-27 10:16:00 -0500

    updating mLab

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const request=require('request');
+const request = require('request');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('./public'));
 
 const databaseUri = 'mongodb://localhost/kudos';
 
-if (process.env.MONGODB_URI){
+if (process.env.MONGODB_URI) {
 	mongoose.connect(process.env.MONGODB_URI);
-}else{
-	mongoose.connect(databaseUri);
+} else {
+	mongoose.connect(databaseUri, { useNewUrlParser: true });
 }
 
 const db = mongoose.connection;
-db.on('error', function(err) {
+db.on('error', function (err) {
 	console.log('Mongoose Error: ', err);
 });
 
-db.once('open', function() {
+db.once('open', function () {
 	console.log('Mongoose connection successful.')
 });
 
@@ -32,6 +31,6 @@ require('./routes/api-routes.js')(app);
 require('./models/Kudo.js');
 require('./models/User.js');
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 	console.log('App running on PORT: ' + PORT);
 });
