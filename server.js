0957161d02482aebf01ae8ac0e2843d340863548
jjeng08 <jjeng08@users.noreply.github.com>
2018-11-27 10:07:35 -0500

    Attaching database to Heroku

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const request=require('request');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('./public'));
 
-mongoose.connect('mongodb://localhost/kudos', {useNewUrlParser: true});
+const databaseUri = 'mongodb://localhost/kudos';
+
+if (process.env.MONGODB_URI){
+	mongoose.connect(process.env.MONGODB_URI);
+}else{
+	mongoose.connect(databaseUri);
+}
+
+const db = mongoose.connection;
+db.on('error', function(err) {
+	console.log('Mongoose Error: ', err);
+});
+
+db.once('open', function() {
+	console.log('Mongoose connection successful.')
+});
 
 require('./routes/api-routes.js')(app);
-
 require('./models/Kudo.js');
 require('./models/User.js');
 
